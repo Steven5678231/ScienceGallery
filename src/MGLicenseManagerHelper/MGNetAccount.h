3467da79be930f9e26e7301beee44a1b7a14974d
eertay <60402519+eertay@users.noreply.github.com>
2021-03-12 13:17:38 -0500

    add face++ files

---
+++ src/MGLicenseManagerHelper/MGNetAccount.h
@@ -0,0 +1,20 @@
+//
+//  MGNetAccount_example.h
+//  FaceppDemo
+//
+//  Created by 张英堂 on 2017/1/12.
+//  Copyright © 2017年 megvii. All rights reserved.
+//
+
+#ifndef MGNetAccount_example_h
+#define MGNetAccount_example_h
+
+
+
+// 访问 https://www.faceplusplus.com.cn， 登录后在控制台生成对应的 key 和 secret 填写到下面的字符串中
+
+#define MG_LICENSE_KEY      @"OpOZVPZrgsTnyPko1ZT7oYoBI97jJiyB" // api_key
+#define MG_LICENSE_SECRET    @"Daspm4xR79PLUbTAA6cK91Ptp-qZXfDo" // api_secret
+
+
+#endif /* MGNetAccount_example_h */
