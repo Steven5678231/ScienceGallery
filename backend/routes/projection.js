866aafd2c14c9e2148035bba22f3f528d34293ad
eertay <60402519+eertay@users.noreply.github.com>
2021-05-26 14:39:45 -0500

    added simple projection page with colours

--- backend/routes/projection.js
+++ backend/routes/projection.js
@@ -5,7 +5,7 @@ var path = require("path");
 /* GET SURVEY PAGE. */
 
 router.get("/", function (req, res, next) {
-  res.sendFile(path.join(__dirname, "..", "/public/projection/template.html"));
+  res.sendFile(path.join(__dirname, "..", "/public/projection/projectionCSS.html"));
 });
 
 module.exports = router;
