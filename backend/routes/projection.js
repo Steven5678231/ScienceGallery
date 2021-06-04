d43eede84c0e1f2b36e393dec326cc16197ad718
eertay <60402519+eertay@users.noreply.github.com>
2021-06-04 12:20:43 -0500

    add routers for separate users

--- backend/routes/projection.js
+++ backend/routes/projection.js
@@ -2,10 +2,21 @@ var express = require("express");
 var router = express.Router();
 var path = require("path");
 
-/* GET SURVEY PAGE. */
+projection_socket = {
+  guest:undefined,
+  host:undefined,
+};
 
-router.get("/", function (req, res, next) {
-  res.sendFile(path.join(__dirname, "..", "/public/projection/projectionCSS.html"));
+router.get("/:user", function (req, res, next) {
+  let user = req.params.user;
+
+  if (user == "host") {
+    res.sendFile(path.join(__dirname, "..", "/public/projection/projectionHost.html"));
+  }
+  else {
+    res.sendFile(path.join(__dirname, "..", "/public/projection/projectionGuest.html"));
+  }
+  
 });
 
 module.exports = router;
