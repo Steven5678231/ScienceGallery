34a662d115b745edecddff9b10531eb70cba4d88
eertay <60402519+eertay@users.noreply.github.com>
2021-06-27 21:17:39 -0500

    added Mel's gradient colour pages

--- backend/routes/projection.js
+++ backend/routes/projection.js
@@ -6,11 +6,11 @@ router.get("/:user", function (req, res, next) {
   let user = req.params.user;
 
   if (user == "host") {
-    res.sendFile(path.join(__dirname, "..", "/public/projection/projectionGIF.html"));
+    res.sendFile(path.join(__dirname, "..", "/public/projection/projectionBlue.html"));
     //res.sendFile(path.join(__dirname, "..", "/public/projection/projectionHost.html"));
   }
   else {
-    res.sendFile(path.join(__dirname, "..", "/public/projection/projectionGuest.html"));
+    res.sendFile(path.join(__dirname, "..", "/public/projection/projectionRed.html"));
   }
 
 });
